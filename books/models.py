043434a013e3ee015984570f8cf9e66b2d56f5a4
Yoqubjonov Abdulaziz <abdulazizyakubjanov27@gmail.com>
2026-04-16 17:39:33 +0500

    2.normative: django practice

--- books/models.py
+++ books/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 
 class BookType(models.TextChoices):
     STANDARD = 'standard'
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(ywe&)wc79u!2&$izfk&t)6ci$n6gc)nl$y5nl@y+#+_e@2p29
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
