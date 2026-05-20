890e8561401a0295ac524051f6f4b6d62d675ae4
Yoqubjonov Abdulaziz <abdulazizyakubjanov27@gmail.com>
2026-05-20 18:30:06 +0500

    9 normative

--- files/models.py
+++ files/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 
-class Order(models.Model):
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('processing', 'Processing'),
-        ('delivered', 'Delivered'),
-        ('cancelled', 'Cancelled'),
-    ]
+# class Order(models.Model):
+#     STATUS_CHOICES = [
+#         ('pending', 'Pending'),
+#         ('processing', 'Processing'),
+#         ('delivered', 'Delivered'),
+#         ('cancelled', 'Cancelled'),
+#     ]
+#
+#     customer_name = models.CharField(max_length=255)
+#     phone = models.CharField(max_length=20)
+#     address = models.TextField()
+#     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+#     is_paid = models.BooleanField(default=False)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return f"{self.customer_name} - {self.status}"
+#
 
-    customer_name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
-    address = models.TextField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-    is_paid = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+from django.db import models
 
-    def __str__(self):
-        return f"{self.customer_name} - {self.status}"
-
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='products/')
--- files/views.py
+++ files/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+
+    return render(request, 'product_list.html', {
+        'products': products
+    })
